03b4cf997d04059fac025a39645f47967c6408f4
konnovK <konnov_k@protonmail.com>
2022-01-10 22:20:47 +0300

    добавил принятие и отклонение заявки в друзья

--- router2/friends.js
+++ router2/friends.js
@@ -9,21 +9,90 @@ friends.use(auth)
 
 
 /**
- * Создать заявку в друзья
+ * Создать или удалить заявку в друзья
  */
 friends.post('/:id', async (req, res) => {
     let id = req.params['id']
 
-    let me = await db.User.findByPk(req._id)
-
-    let user = await db.User.findByPk(id)
-
-    await db.FriendRequest.create({
-        UserId: req._id,
-        FriendId: id,
-        status: 'sent'
+    let request = await db.FriendRequest.findOne({
+        where: {
+            [Op.or]: [
+                {
+                    UserId: req._id,
+                    FriendId: id
+                },
+                {
+                    UserId: id,
+                    FriendId: req._id
+                }
+            ]
+        }
     })
+    if (request) {
+        if (request.status === 'accepted') {
+            return res.status(400).json({message: 'request is already accepted'})
+        }
+        if (request.status === 'rejected') {
+            return res.status(400).json({message: 'request is already rejected'})
+        }
+        await request.destroy()
+    } else {
+        await db.FriendRequest.create({
+            UserId: req._id,
+            FriendId: id,
+            status: 'sent'
+        })
+    }
+
+
     return res.send()
+})
+
+
+/**
+ * Принять заявку в друзья
+ */
+friends.patch('/:id/accept', async (req, res) => {
+    let id = req.params['id']
+
+    let request = await db.FriendRequest.findOne({
+        where: {
+            UserId: id,
+            FriendId: req._id
+        }
+    })
+
+    if (!request) {
+        return res.status(400).json({message: 'request is none'})
+    }
+
+    await request.update({status: 'accepted'})
+
+    res.send()
+})
+
+
+
+/**
+ * Отклонить заявку в друзья
+ */
+friends.patch('/:id/reject', async (req, res) => {
+    let id = req.params['id']
+
+    let request = await db.FriendRequest.findOne({
+        where: {
+            UserId: id,
+            FriendId: req._id
+        }
+    })
+
+    if (!request) {
+        return res.status(400).json({message: 'request is none'})
+    }
+
+    await request.update({status: 'rejected'})
+
+    res.send()
 })
 
 module.exports = friends
