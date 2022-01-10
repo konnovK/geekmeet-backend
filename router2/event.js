c251996adf84dea085176a7ee56e5a1e79475fef
LOSTSVUND <56744129+ilnarmirkhaev@users.noreply.github.com>
2022-01-10 22:30:24 +0300

    Фикс отправки/отмены заявки на ивент

--- router2/event.js
+++ router2/event.js
@@ -251,7 +251,7 @@ event.delete('/:id', async (req, res) => {
 
 
 /**
- * Подать заявку на ивент
+ * Подать или отменить заявку на ивент
  */
 event.post('/:id/request', async (req, res) => {
     let _id = req._id;
@@ -266,9 +266,15 @@ event.post('/:id/request', async (req, res) => {
         where: {
             UserId: _id,
             EventId: event.id,
+            status: 'sent'
         }
     })) {
-        return res.status(400).json({message: 'request already exists'})
+        await db.JoinRequest.destroy({
+            where: {
+                UserId: _id,
+                EventId: event.id,
+            }
+        })
     } else {
         await db.JoinRequest.create({
             UserId: _id,
@@ -276,30 +282,6 @@ event.post('/:id/request', async (req, res) => {
             status: 'sent'
         })
     }
-
-    res.json()
-})
-
-
-
-/**
- * Отменить заявку на ивент
- */
-event.delete('/:id/request', async (req, res) => {
-    let _id = req._id;
-    let event = await db.Event.findByPk(req.params['id'])
-
-    // Валидация
-    if (!_id || event.creatorId === _id) {
-        return res.status(401).json({message: 'authorization error'})
-    }
-
-    await db.JoinRequest.destroy({
-        where: {
-            UserId: _id,
-            EventId: event.id,
-        }
-    })
 
     res.json()
 })
